8707a4f1e5261a7c2213d5606e559eef4eb39bc2
deep23-hub <82639710+deep23-hub@users.noreply.github.com>
2021-11-18 14:24:05 +0530

    Add files via upload

---
+++ swapFile.py
@@ -0,0 +1,17 @@
+def swap ():
+    file1 = input("Enter your file name")
+    file2 = input("Enter your file name")
+
+    with open(file1, 'r') as a : 
+        dataA = a.read()
+
+    with open(file2, 'r') as b : 
+        dataB = b.read()    
+    
+    with open(file1, 'w') as a : 
+        a.write(dataB)
+
+    with open(file2, 'w') as b : 
+        b.write(dataA)     
+
+swap() 
